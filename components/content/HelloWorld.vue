d33317bf6be73179f3f5071003ba90d7da37c38f
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2023-01-23 17:32:59 +0000

    commit

--- components/content/HelloWorld.vue
+++ components/content/HelloWorld.vue
@@ -1,5 +1,8 @@
-<template>
+s<template>
   <div>
+    <Head>
+      <Title>Guillaume Duhan</Title>
+    </Head>
     <!-- https://content.nuxtjs.org/api/components/content-slot -->
     <ContentSlot :use="$slots.default" unwrap="p" />
   </div>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "Guillaume Duhan, CTO & Front-end Developer",
+      meta: [
+        // <meta name="description" content="My amazing site">
+        {
+          name: "description",
+          content: "Guillaume Duhan is a front-end developer, CTO & author.",
+        },
+      ],
+    },
+  },
   extends: "content-wind",
   content: {
     highlight: {
